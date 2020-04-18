6a0b89a780ab5c809d839e3d93672793194d383a
Alberto del Valle <alberto.dvc@gmail.com>
2020-04-18 21:05:17 +0200

    Update basic info about the project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alberto del Valle | Frontend developer`,
+    description: `Hi, I am Alberto del Valle, I am a frontend developer and sound engineer`,
+    author: `Alberto del Valle`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
